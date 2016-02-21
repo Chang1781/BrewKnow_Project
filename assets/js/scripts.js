9f64eaeb407265c351c942288d963c2de8aa963a
OSP123 <omarpatel123@gmail.com>
2016-02-20 16:00:05 -0800

    created bubble effect

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,89 +1,40 @@
-$(document).ready(function (){  
-  var $bubbles = $('.bubbles');
+jQuery.fn.verticalMarquee = function(vertSpeed, horiSpeed, index) {
+   
+    this.css('float', 'left');
 
-  function bubbles() {
-    
-    // Settings
-    var min_bubble_count = 20, // Minimum number of bubbles
-        max_bubble_count = 60, // Maximum number of bubbles
-        min_bubble_size = 3, // Smallest possible bubble diameter (px)
-        max_bubble_size = 12; // Maximum bubble blur amount (px)
-    
-    // Calculate a random number of bubbles based on our min/max
-    var bubbleCount = min_bubble_count + Math.floor(Math.random() * (max_bubble_count + 1));
-    
-    // Create the bubbles
-    for (var i = 0; i < bubbleCount; i++) {
-      $bubbles.append('<div class="bubble-container"><div class="bubble"></div></div>');
-    }
-    
-    // Now randomise the various bubble elements
-    $bubbles.find('.bubble-container').each(function(){
-      
-      // Randomise the bubble positions (0 - 100%)
-      var pos_rand = Math.floor(Math.random() * 101);
-      
-      // Randomise their size
-      var size_rand = min_bubble_size + Math.floor(Math.random() * (max_bubble_size + 1));
-      
-      // Randomise the time they start rising (0-15s)
-      var delay_rand = Math.floor(Math.random() * 16);
-      
-      // Randomise their speed (3-8s)
-      var speed_rand = 3 + Math.floor(Math.random() * 9);
-      
-      // Random blur
-      var blur_rand = Math.floor(Math.random() * 3);
-      
-      // Cache the this selector
-      var $this = $(this);
-      
-      // Apply the new styles
-      $this.css({
-        'left' : pos_rand + '%',
-        
-        '-webkit-animation-duration' : speed_rand + 's',
-        '-moz-animation-duration' : speed_rand + 's',
-        '-ms-animation-duration' : speed_rand + 's',
-        'animation-duration' : speed_rand + 's',
-        
-        '-webkit-animation-delay' : delay_rand + 's',
-        '-moz-animation-delay' : delay_rand + 's',
-        '-ms-animation-delay' : delay_rand + 's',
-        'animation-delay' : delay_rand + 's',
-        
-        '-webkit-filter' : 'blur(' + blur_rand  + 'px)',
-        '-moz-filter' : 'blur(' + blur_rand  + 'px)',
-        '-ms-filter' : 'blur(' + blur_rand  + 'px)',
-        'filter' : 'blur(' + blur_rand  + 'px)',
-      });
-      
-      $this.children('.bubble').css({
-        'width' : size_rand + 'px',
-        'height' : size_rand + 'px'
-      });
-      
-    });
-  }
+    vertSpeed = vertSpeed || 1;
+    horiSpeed = 1/horiSpeed || 1;
 
-  // In case users value their laptop battery life
-  // Allow them to turn the bubbles off
-  $('.bubble-toggle').click(function(){
-    if($bubbles.is(':empty')) {
-      bubbles();
-      $bubbles.show();
-      $(this).text('Bubbles Off');
-    } else {
-      $bubbles.fadeOut(function(){
-        $(this).empty();
-      });
-      $(this).text('Bubbles On');
-    }
-    
-    return false;
-  });
+    var windowH = this.parent().height(),
+        thisH = this.height(),
+        parentW = (this.parent().width() - this.width()) / 2,
+        rand = Math.random() * (index * 1000),
+        current = this;
 
-  bubbles();
+    this.css('margin-top', windowH + thisH);
+    this.parent().css('overflow', 'hidden');
 
+    setInterval(function() {
+        current.css({
+            marginTop: function(n, v) {
+                return parseFloat(v) - vertSpeed;
+            },
+            marginLeft: function(n, v) {
+                return (Math.sin(new Date().getTime() / (horiSpeed * 1000) + rand) + 1) * 45;
+            }
+        });
+    }, 15);
+
+    setInterval(function() {
+        if (parseFloat(current.css('margin-top')) < -thisH) {
+            current.css('margin-top', windowH + thisH);
+        }
+    }, 250);
+};
+var message = 1;
+$('.message').each(function(message) {  
+    $(this).verticalMarquee(1, 1, message);
+    message++
 });
+
 
